3b5f501731257c50ea3b9e24a876cc42ae2b2979
Oliver Njiru <51198797+olivernjiru@users.noreply.github.com>
2022-06-05 20:24:48 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,2 @@
+// get element by class 'menu-btn' and a click event listener to toggle the 'main-menu' class to 'show' 
 document.querySelector('.menu-btn').addEventListener('click', () => document.querySelector('.main-menu').classList.toggle('show'))
